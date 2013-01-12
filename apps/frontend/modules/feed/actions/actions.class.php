44d76ae201eed22cdacb5382fd089cbb65719693
Guillaume Marsay <gmarsay@gmail.com>
2013-01-12 12:04:05 +0100

    Mise a jour de la page de maintenance

--- apps/frontend/modules/feed/actions/actions.class.php
+++ apps/frontend/modules/feed/actions/actions.class.php
@@ -5,9 +5,10 @@
  *
  * @package    lufy
  * @subpackage feed
- * @author     Your name here
+ * @author     Guillaume Marsay
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
+
 class feedActions extends sfActions
 {
  /**
@@ -17,32 +18,34 @@ class feedActions extends sfActions
   */
   public function executeRss2()
   {
-  $feed = new sfRss201Feed();
- 
-  $feed->setTitle('Gamers Assembly');
-  $feed->setLink('http://www.gamers-assembly.net/');
-  $feed->setAuthorEmail('contact@futurolan.net');
-  $feed->setAuthorName('Association Futurolan');
- 
-  $newss = Doctrine_Query::create()
-                        ->select('*')
-                        ->from('news')
-                        ->where('status = 1')
-                        ->orderby('publish_on DESC')
-                        ->limit('limit', 10)
-                        ->execute();
-  foreach ($newss as $news)
-  {
-    $item = new sfFeedItem();
-    $item->setTitle($news->getTitle());
-    $item->setLink('news/view?slug='.$news->getSlug());
-    $item->setPubdate(strtotime($news->getPublishOn()));
-    $item->setUniqueId($news->getSlug());
-    $item->setDescription(substr($news->getContent(), 0, 400));
- 
-    $feed->addItem($item);
-  }
- 
-  $this->feed = $feed;
+    $feed = new sfRss201Feed();
+
+    $feed->setTitle('Gamers Assembly');
+    $feed->setLink('http://www.gamers-assembly.net/');
+    $feed->setAuthorEmail('contact@futurolan.net');
+    $feed->setAuthorName('Association Futurolan');
+
+    $newss = Doctrine_Query::create()
+      ->select('*')
+      ->from('news')
+      ->where('status = 1')
+      ->andWhere('slug NOT LIKE "%-en"')
+      ->orderby('publish_on DESC')
+      ->limit('limit', 10)
+      ->execute();
+
+    foreach ($newss as $news)
+    {
+      $item = new sfFeedItem();
+      $item->setTitle($news->getTitle());
+      $item->setLink('news/view?slug='.$news->getSlug());
+      $item->setPubdate(strtotime($news->getPublishOn()));
+      $item->setUniqueId($news->getSlug());
+      $item->setDescription(substr($news->getContent(), 0, 400));
+
+      $feed->addItem($item);
+    }
+
+    $this->feed = $feed;
   }
 }
--- web/maintenance.php
+++ web/maintenance.php
@@ -1,28 +1,74 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Gamers Assembly</title>
-	<style>
-	body {
-		margin: 0px;
-		padding: 0px;
-	}
-	
-	#Page {
-		width: 800px;
-		height: 600px;
-		margin: auto auto;
-		margin-top: 30px;
-		box-shadow: 0px 0px 20px #000;
-	}
-	</style>
+  <title>Gamers Assembly 2013</title>
+  <style>
+  <!--
+  body {
+    background-color: #000000;
+    color: #fff;
+  }
+  #logo {
+    text-align: center;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: 10;
+    width: 100%;
+  }
+  #ouverture {
+    text-align: center;
+    position: fixed;
+    bottom: 30px;
+    right: 10px;
+    z-index: 10;
+  }
+  #facebook {
+    position: fixed;
+    bottom: 40px;
+    left: 20px;
+    z-index: 10;
+    width: 100%;
+  }
+  #player {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+  }
+  -->
+  </style>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 </head>
 <body>
-<!-- Pour une fois ce n'est pas un bug :) -->
-	<div id="Page">
-		<img src="/splash_opening.jpg" border="0"/>
-	</div>
-  <script type="text/javascript">
+  <div id="logo">
+    <img src="logo.png" />
+  </div>
+  
+  <div id="ouverture">
+    <img src="ouverture.png" />
+  </div>
+  
+  <div id="facebook">
+    <div id="fb-root"></div>
+    <script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=183148825128872";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+    <div class="fb-like" data-href="http://www.gamers-assembly.net/" data-send="true" data-width="500" data-show-faces="true" data-colorscheme="dark"></div>
+  </div>
+  
+  <div id="player">
+    <iframe frameborder="0" width="100%" height="100%" src="http://www.dailymotion.com/embed/video/xwnkua?logo=0&autoPlay=1"></iframe>
+  </div>
+  
+  <script type="text/javascript"><!--
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-183270-3']);
     _gaq.push(['_trackPageview']);
