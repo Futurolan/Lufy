878290a03b9e3eaeafb35cff2a585541bedf62c2
Guillaume Marsay <gmarsay@gmail.com>
2013-05-11 19:31:32 +0200

    Corrections diverses Correction du model Block dans le schema Mise a jour de la page de maintenance Indentation du fichier BaseActions.class.php Suppression de fichiers inutiles

--- lib/action/BaseActions.class.php
+++ lib/action/BaseActions.class.php
@@ -1,29 +1,22 @@
 <?php
-
 class BaseActions extends sfActions
 {
-  /**
-   * Process a form if needed with given get/post parameter name
-   * 
-   * @param sfWebRequest $request
-   * @param string       $parameterName
-   * @return boolean
-   */
   protected function embeddedProcessForm(sfWebRequest $request, $parameterName)
   {
     if($request->isMethod('post'))
     {
       $this->form->bind($request->getParameter($parameterName));
-     
-			if($this->form->isValid())
+      if($this->form->isValid())
       {
-				if(method_exists($this->form, 'save')) $this->form->save();
-        
+        if(method_exists($this->form, 'save'))
+        {
+          $this->form->save();
+        }
+
         return True;
       }
     }
-    
+
     return False;
   }
-
 }
--- web/maintenance.php
+++ web/maintenance.php
@@ -1,82 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Gamers Assembly 2013</title>
+  <title>Gamers Assembly</title>
   <style>
-  <!--
-  body {
-    background-color: #000000;
-    color: #fff;
-  }
-  #logo {
-    text-align: center;
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    z-index: 10;
-    width: 100%;
-  }
-  #ouverture {
-    text-align: center;
-    position: fixed;
-    bottom: 30px;
-    right: 10px;
-    z-index: 10;
-  }
-  #facebook {
-    position: fixed;
-    bottom: 40px;
-    left: 20px;
-    z-index: 10;
-    width: 100%;
-  }
-  #player {
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-  }
-  -->
+    #info {
+      text-align: center;
+      margin-top: 100px;
+    }
   </style>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 </head>
 <body>
-  <div id="logo">
-    <img src="logo.png" />
-  </div>
-  
-  <div id="ouverture">
-    <img src="ouverture.png" />
-  </div>
-  
-  <div id="facebook">
-    <div id="fb-root"></div>
-    <script>
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=183148825128872";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-    <div class="fb-like" data-href="http://www.gamers-assembly.net/" data-send="true" data-width="500" data-show-faces="true" data-colorscheme="dark"></div>
-  </div>
-  
-  <div id="player">
-    <iframe frameborder="0" width="100%" height="100%" src="http://www.dailymotion.com/embed/video/xwnkua?logo=0&autoPlay=1"></iframe>
-  </div>
-  
-  <script type="text/javascript"><!--
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-183270-3']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
+<div id="info">
+  <h1>Maintenance en cours</h1>
+  <h2>Le site sera de retour dans quelques instants...</h2>
+</div>
 </body>
 </html>
