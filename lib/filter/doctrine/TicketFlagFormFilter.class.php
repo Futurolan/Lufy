114e6faf701ffe4883050e700941ed46215958cd
Guillaume Marsay <gmarsay@gmail.com>
2013-05-13 22:14:33 +0200

    Mise a jour du schema Ajout des modeles permettant de gerer le systeme de support par ticket

---
+++ lib/filter/doctrine/TicketFlagFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * TicketFlag filter form.
+ *
+ * @package    lufy
+ * @subpackage filter
+ * @author     Guillaume Marsay
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class TicketFlagFormFilter extends BaseTicketFlagFormFilter
+{
+  public function configure()
+  {
+  }
+}
