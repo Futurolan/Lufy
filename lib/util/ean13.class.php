a7c10d1ba5a784e20aa6bd47c15d34f4375bd1ff
Bardot <Geek_Zone@hotmail.com>
2013-05-23 14:28:52 +0200

    Refactoration de la classe ean. #21

--- lib/util/ean13.class.php
+++ lib/util/ean13.class.php
@@ -1,82 +1,135 @@
 <?php
+    class Ean13
+    {
+      protected $current = null;
 
-/**
- * Gestion des codes barres EAN13
- * Guillaume Marsay <guillaume@futurolan.net>
- * Association Futurolan <contact@futurolan.net>
- */
-
-class Ean13{
-	private $ean;
-	public static $lastGen = '1000000000009';
-
-	private static function checksum($number){
-		for ($i = 0; $i < 12; $i++)
-		{
-      	$EAN13[$i] = substr($number, $i, 1);
+      public function __construct($ean)
+      {
+        if (strlen($ean) == 12)
+        {
+          // Calcul de la clef de controle
+          // Definition de $current
+          $this->current = $ean;
+          $this->checksum();
+        }
+        elseif (strlen($ean) == 13)
+        {
+          // Verifie la validite de la clef de controle
+          // Definition de $current
+          if ($this->isvalid($ean))
+          {
+          	$this->current = $ean
+          }
+        }
+        else
+        {
+          throw new Exception('Erreur - Code invalide');
+        }
       }
-      $pair = 0;
-      for ($u = 0; $u < 12; $u = $u + 2) {
-        	$pair = $pair + $EAN13[$u];
+
+      public function get()
+      {
+        return $this->current;
       }
-      $impair = 0;
-      for ($y = 1; $y < 12; $y = $y + 2) {
-      	$impair = $impair + $EAN13[$y] * 3;
+
+      public function prev()
+      {
+        // Genere le precedent code ean13
+        // Mise a jour de la valeur de $current
+        $number = substr($this->current, 0, 12);
+        $newNumber = $number + 1;
+        for ($i = 0; $i < 12; $i++)
+        {
+          $EAN13[$i] = substr($newNumber, $i, 1);
+      	}
+      	$pair = 0;
+      	for ($u = 0; $u < 12; $u = $u + 2) {
+          $pair = $pair + $EAN13[$u];
+      	}
+      	$impair = 0;
+      	for ($y = 1; $y < 12; $y = $y + 2) {
+          $impair = $impair + $EAN13[$y] * 3;
+      	}
+      	$total = $pair + $impair;
+      	$r = fmod($total, 10);
+      	$controlkey = 10 - $r;
+      	$this->current = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
+        return $this;
+        return $this;
       }
-      $total = $pair + $impair;
-      $r = fmod($total, 10);
-      $controlkey = 10 - $r;
-      $nextEan = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
 
-      return $nextEan;
-	}
+      public function next()
+      {
+        // Genere le prochain code ean13
+        // Mise a jour de la valeur de $current
+        $number = substr($this->current, 0, 12);
+        $newNumber = $number + 1;
+        for ($i = 0; $i < 12; $i++)
+        {
+          $EAN13[$i] = substr($newNumber, $i, 1);
+      	}
+      	$pair = 0;
+      	for ($u = 0; $u < 12; $u = $u + 2) {
+          $pair = $pair + $EAN13[$u];
+      	}
+      	$impair = 0;
+      	for ($y = 1; $y < 12; $y = $y + 2) {
+          $impair = $impair + $EAN13[$y] * 3;
+      	}
+      	$total = $pair + $impair;
+      	$r = fmod($total, 10);
+      	$controlkey = 10 - $r;
+      	$this->current = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
+        return $this;
+      }
 
-   public static function nextEan()
-   {
-	   $number = substr(Ean13::$lastGen, 0, 12);
-      $newNumber = $number + 1;
-	   $nextEan = Ean13::checksum($newNumber);
-		return $nextEan;
-   }
+      // Verifie la clef de controle de $ean ; return true ou false en fonction du resultat
+      public function isValid($ean)
+      {
+        $result = false;
+        $number = substr($ean, 0, 12);
+        for ($i = 0; $i < 12; $i++)
+        {
+          $EAN13[$i] = substr($number, $i, 1);
+      	}
+      	$pair = 0;
+      	for ($u = 0; $u < 12; $u = $u + 2) {
+          $pair = $pair + $EAN13[$u];
+      	}
+      	$impair = 0;
+      	for ($y = 1; $y < 12; $y = $y + 2) {
+          $impair = $impair + $EAN13[$y] * 3;
+      	}
+      	$total = $pair + $impair;
+      	$r = fmod($total, 10);
+      	$controlkey = 10 - $r;
+      	if($controlkey ==substr($ean, -1, $length = null);) {
+          $result = true;
+      	}
+      	return $result;
+      }
 
-	static function raz(){
-		Ean13::$lastGen = 1000000000009;
-	}
+      // Calcul de la clef de controle a partie de $current
+      // Mise a jour de la valeur de $current
+      private function checksum()
+      {
+        for ($i = 0; $i < 12; $i++)
+        {
+          $EAN13[$i] = substr($this->current, $i, 1);
+      	}
+      	$pair = 0;
+      	for ($u = 0; $u < 12; $u = $u + 2) {
+          $pair = $pair + $EAN13[$u];
+      	}
+      	$impair = 0;
+      	for ($y = 1; $y < 12; $y = $y + 2) {
+          $impair = $impair + $EAN13[$y] * 3;
+      	}
+      	$total = $pair + $impair;
+      	$r = fmod($total, 10);
+      	$controlkey = 10 - $r;
+      	$this->current = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
+      }
 
-	public function __construct() {
-		$this->ean = Ean13::nextEan();
-		Ean13::$lastgener = $this->ean;
-	}
-
-	public function __construct($prevEan) {
-		$this->ean = nextEanParam($prevEan);
-		Ean13::$lastgener = $this->ean;
-	}
-
-	public function getEan() {
-		return $this->ean;
-	}
-
-	public function getNumber()
-	{
-		$number = substr($this->ean, 0, 12);
-		return $number;
-	}
-
-	public function getChecksum()
-	{
-		$checksum = substr($this->ean, -1, $length = null);
-		return $checksum;
-	}
-
-   public static function nextEanParam($number)
-   {
-		$number = substr($this->ean, 0, 12);
-		$newNumber = $number + 1;
-		$nextEan = Ean13::checksum($newNumber);
-		return $nextEan;
-   }
-
-}
-
+    }
 ?>
