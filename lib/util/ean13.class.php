b4c4525a009337e90e407100cca7778bcbb75768
Bardot <Geek_Zone@hotmail.com>
2013-05-23 11:29:04 +0200

    Ajout de méthodes a la classe ean. #21

--- lib/util/ean13.class.php
+++ lib/util/ean13.class.php
@@ -6,49 +6,77 @@
  * Association Futurolan <contact@futurolan.net>
  */
 
-class Ean13Tool{
-	public static $lastreneger = '1000000000001';
-	private $myEan;
+class Ean13{
+	private $ean;
+	public static $lastGen = '1000000000009';
 
-	public function __construct() {
-		/*$this->myEan = '1000000000014';*/
-		$this->myEan = $this->nextEan($lastgener);
-		Ean::$lastgener = $this->myEan;
+	private static function checksum($number){
+		for ($i = 0; $i < 12; $i++)
+		{
+      	$EAN13[$i] = substr($number, $i, 1);
+      }
+      $pair = 0;
+      for ($u = 0; $u < 12; $u = $u + 2) {
+        	$pair = $pair + $EAN13[$u];
+      }
+      $impair = 0;
+      for ($y = 1; $y < 12; $y = $y + 2) {
+      	$impair = $impair + $EAN13[$y] * 3;
+      }
+      $total = $pair + $impair;
+      $r = fmod($total, 10);
+      $controlkey = 10 - $r;
+      $nextEan = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
+
+      return $nextEan;
 	}
 
-	static function initialisation () {
-
-	}
+   public static function nextEan()
+   {
+	   $number = substr(Ean13::$lastGen, 0, 12);
+      $newNumber = $number + 1;
+	   $nextEan = Ean13::checksum($newNumber);
+		return $nextEan;
+   }
 
 	static function raz(){
-		Ean::$lastgener = 1000000000001;
+		Ean13::$lastGen = 1000000000009;
+	}
+
+	public function __construct() {
+		$this->ean = Ean13::nextEan();
+		Ean13::$lastgener = $this->ean;
+	}
+
+	public function __construct($prevEan) {
+		$this->ean = nextEanParam($prevEan);
+		Ean13::$lastgener = $this->ean;
 	}
 
 	public function getEan() {
-		return $this->myEan;
+		return $this->ean;
 	}
 
-   public function nextEan($string) {
-        $k = substr($string, 0, 12);
-        $k = $k + 1;
-        for ($i = 0; $i < 12; $i++) {
-            $EAN13[$i] = substr($k, $i, 1);
-        }
-        $pair = 0;
-        for ($u = 0; $u < 12; $u = $u + 2) {
-            $pair = $pair + $EAN13[$u];
-        }
-        $impair = 0;
-        for ($y = 1; $y < 12; $y = $y + 2) {
-            $impair = $impair + $EAN13[$y] * 3;
-        }
-        $total = $pair + $impair;
-        $r = fmod($total, 10);
-        $controlkey = 10 - $r;
-        $n = \str_pad($k, 13, $controlkey, STR_PAD_RIGHT);
+	public function getNumber()
+	{
+		$number = substr($this->ean, 0, 12);
+		return $number;
+	}
 
-        return $n;
-    }
+	public function getChecksum()
+	{
+		$checksum = substr($this->ean, -1, $length = null);
+		return $checksum;
+	}
+
+   public static function nextEanParam($number)
+   {
+		$number = substr($this->ean, 0, 12);
+		$newNumber = $number + 1;
+		$nextEan = Ean13::checksum($newNumber);
+		return $nextEan;
+   }
+
 }
 
 ?>
