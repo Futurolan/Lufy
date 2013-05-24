fbb4a341e751088a49b3b80b832fed8feee18474
Bardot <Geek_Zone@hotmail.com>
2013-05-24 09:28:15 +0200

    Correction de la classe ean mise ne place de celle de guillaume

--- lib/util/ean13.class.php
+++ lib/util/ean13.class.php
@@ -1,134 +1,119 @@
 <?php
-    class Ean13
-    {
-      protected $current = null;
+  class Ean13
+  {
+    protected $current = null;
 
-      public function __construct($ean)
+    public function __construct($ean)
+    {
+      if (strlen($ean) == 12)
       {
-        if (strlen($ean) == 12)
+        $this->current = $this->checksum($ean);
+      }
+      elseif (strlen($ean) == 13)
+      {
+        if ($this->isValid($ean))
         {
-          // Calcul de la clef de controle
-          // Definition de $current
           $this->current = $ean;
-          $this->checksum();
-        }
-        elseif (strlen($ean) == 13)
-        {
-          // Verifie la validite de la clef de controle
-          // Definition de $current
-          if ($this->isvalid($ean))
-          {
-          	$this->current = $ean;
-          }
         }
         else
         {
           throw new Exception('Erreur - Code invalide');
         }
       }
-
-      public function get()
+      else
       {
-        return $this->current;
-      }
-
-      public function prev()
-      {
-        // Genere le precedent code ean13
-        // Mise a jour de la valeur de $current
-        $number = substr($this->current, 0, 12);
-        $newNumber = $number + 1;
-        for ($i = 0; $i < 12; $i++)
-        {
-          $EAN13[$i] = substr($newNumber, $i, 1);
-      	}
-      	$pair = 0;
-      	for ($u = 0; $u < 12; $u = $u + 2) {
-          $pair = $pair + $EAN13[$u];
-      	}
-      	$impair = 0;
-      	for ($y = 1; $y < 12; $y = $y + 2) {
-          $impair = $impair + $EAN13[$y] * 3;
-      	}
-      	$total = $pair + $impair;
-      	$r = fmod($total, 10);
-      	$controlkey = 10 - $r;
-      	$this->current = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
-        return $this;
-        return $this;
-      }
-
-      public function next()
-      {
-        // Genere le prochain code ean13
-        // Mise a jour de la valeur de $current
-        $number = substr($this->current, 0, 12);
-        $newNumber = $number + 1;
-        for ($i = 0; $i < 12; $i++)
-        {
-          $EAN13[$i] = substr($newNumber, $i, 1);
-      	}
-      	$pair = 0;
-      	for ($u = 0; $u < 12; $u = $u + 2) {
-          $pair = $pair + $EAN13[$u];
-      	}
-      	$impair = 0;
-      	for ($y = 1; $y < 12; $y = $y + 2) {
-          $impair = $impair + $EAN13[$y] * 3;
-      	}
-      	$total = $pair + $impair;
-      	$r = fmod($total, 10);
-      	$controlkey = 10 - $r;
-      	$this->current = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
-        return $this;
-      }
-
-      // Verifie la clef de controle de $ean ; return true ou false en fonction du resultat
-      public function isValid($ean)
-      {
-        $result = false;
-        $number = substr($ean, 0, 12);
-        for ($i = 0; $i < 12; $i++)
-        {
-          $EAN13[$i] = substr($number, $i, 1);
-      	}
-      	$pair = 0;
-      	for ($u = 0; $u < 12; $u = $u + 2) {
-          $pair = $pair + $EAN13[$u];
-      	}
-      	$impair = 0;
-      	for ($y = 1; $y < 12; $y = $y + 2) {
-          $impair = $impair + $EAN13[$y] * 3;
-      	}
-      	$total = $pair + $impair;
-      	$r = fmod($total, 10);
-      	$controlkey = 10 - $r;
-      	if($controlkey == substr($ean, -1, $length = null)){
-          $result = true;
-      	}
-      	return $result;
-      }
-
-      // Calcul de la clef de controle a partie de $current
-      // Mise a jour de la valeur de $current
-      private function checksum()
-      {
-        for ($i = 0; $i < 12; $i++)
-        {
-          $EAN13[$i] = substr($this->current, $i, 1);
-      	}
-      	$pair = 0;
-      	for ($u = 0; $u < 12; $u = $u + 2) {
-          $pair = $pair + $EAN13[$u];
-      	}
-      	$impair = 0;
-      	for ($y = 1; $y < 12; $y = $y + 2) {
-          $impair = $impair + $EAN13[$y] * 3;
-      	}
-      	$total = $pair + $impair;
-      	$r = fmod($total, 10);
-      	$controlkey = 10 - $r;
-      	$this->current = \str_pad($number, 13, $controlkey, STR_PAD_RIGHT);
+        throw new Exception('Erreur - Code invalide');
       }
     }
+
+
+    public function get()
+    {
+      return $this->current;
+    }
+
+
+    public function prev()
+    {
+      $prev = substr($this->current, 0, 12);
+      $prev--;
+      $prev = $this->checksum($prev);
+
+      $this->current = $prev;
+
+      return $this;
+    }
+
+
+    public function next()
+    {
+      $next = substr($this->current, 0, 12);
+      $next++;
+      $next = $this->checksum($next);
+
+      $this->current = $next;
+
+      return $this;
+    }
+
+
+    public function isValid($ean)
+    {
+      $ean_checked = substr($ean, 0, 12);
+      $ean_checked = $this->checksum($ean_checked);
+
+      if ($ean == $ean_checked)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+
+    }
+
+
+    private function checksum($number)
+    {
+      $array_number = str_split($number, 1);
+
+      $pair = 0;
+      for ($i = 0; $i < 12; $i+= 2)
+      {
+        $pair+= $array_number[$i] * 1;
+      }
+
+      $impair = 0;
+      for ($j = 1; $j < 12; $j+= 2)
+      {
+        $impair+= $array_number[$j] * 3;
+      }
+
+      $total = $pair + $impair;
+      $r = fmod($total, 10);
+      $controlkey = 10 - $r;
+
+      if ($controlkey == 10)
+      {
+        $controlkey = 0;
+      }
+
+      $ean = $number.$controlkey;
+
+      return $ean;
+    }
+  }
 ?>
+
+<?php
+  // Test de la classe
+  $var = 100000000001;
+
+  for ($i=$var; $i<$var+20; $i++)
+  {
+    $ean = new Ean13($i);
+    echo '<b>Test de '.$i.'</b><br/>Current : '.$ean->get().'<br/>Next : '.$ean->next()->get().'<br/>Prev : '.$ean->prev()->prev()->get().'<br/><br/>';
+  }
+?>
+
