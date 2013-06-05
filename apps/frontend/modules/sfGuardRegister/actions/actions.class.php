29b527ca151e6574532c93a272395fd8b4d5c7e5
Guillaume Marsay <gmarsay@gmail.com>
2013-06-05 22:27:44 +0200

    Finalisation du module de creation de compte utilisateur : * Controle des donnees saisies dans le champs username * Ajout d'une validation via reCaptcha
    Close #14

---
+++ apps/frontend/modules/sfGuardRegister/actions/actions.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class sfGuardRegisterActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    if ($this->getUser()->isAuthenticated())
+    {
+      $this->getUser()->setFlash('notice', 'You are already registered and signed in!');
+      $this->redirect('@homepage');
+    }
+
+    $this->form = new sfGuardRegisterForm();
+
+    if ($request->isMethod('post'))
+    {
+      $captcha = array(
+        'recaptcha_challenge_field' => $request->getParameter('recaptcha_challenge_field'),
+        'recaptcha_response_field'  => $request->getParameter('recaptcha_response_field'),
+      );
+
+      $this->form->bind(array_merge($request->getParameter($this->form->getName()), array('captcha' => $captcha)));
+      //$this->form->bind($request->getParameter($this->form->getName()));
+      if ($this->form->isValid())
+      {
+        $user = $this->form->save();
+        $this->getUser()->signIn($user);
+
+        $this->redirect('@homepage');
+      }
+    }
+  }
+}
--- lib/form/sfGuardRegisterForm.class.php
+++ lib/form/sfGuardRegisterForm.class.php
@@ -14,56 +14,137 @@ class sfGuardRegisterForm extends BaseSfGuardUserForm {
 
   public function configure()
   {
-    unset($this['algorithm'], $this['salt'], $this['group_list'], $this['permissions_list'], $this['groups_list'], $this['is_active'], $this['is_super_admin'], $this['last_login'], $this['created_at'], $this['updated_at']);
-
-    $this->validatorSchema->setPostValidator(
-      new sfValidatorAnd(array(
-        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('email_address'))),
-        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('username'))),
-        new sfValidatorSchemaCompare('password',  sfValidatorSchemaCompare::EQUAL, 'password_again'),
-        new sfValidatorReCaptcha(array('private_key' => sfConfig::get('app_recaptcha_private_key'))),
-      ))
+    unset(
+      $this['algorithm'],
+      $this['salt'],
+      $this['group_list'],
+      $this['permissions_list'],
+      $this['groups_list'],
+      $this['is_active'],
+      $this['is_super_admin'],
+      $this['last_login'],
+      $this['created_at'],
+      $this['updated_at']
     );
+
 
     $this->setWidget('password', new sfWidgetFormInputPassword());
     $this->setWidget('password_again', new sfWidgetFormInputPassword());
 
-    $this->widgetSchema['captcha'] = new sfWidgetFormReCaptcha(array('public_key' => sfConfig::get('app_recaptcha_public_key')));
+    $this->setWidget('captcha', new sfWidgetFormReCaptcha(array('public_key' => sfConfig::get('app_recaptcha_public_key'))));
 
-    $this->setValidator('first_name', new sfValidatorString(array('required' => true, 'min_length' => 4, 'max_length' => 255)));
-    $this->setValidator('last_name', new sfValidatorString(array('required' => true, 'min_length' => 4, 'max_length' => 255)));
-    $this->setValidator('email_address', new sfValidatorEmail(array('required' => true),array('required' => 'L\'adresse de courriel est requise','invalid' => 'Ce courriel n\'est pas valide')));
-    $this->setValidator('username', new sfValidatorAnd(
-                array(
-                    new sfValidatorString(
-                            array('required' => true, 'min_length' => 2, 'max_length' => 15),
-                            array(
-                                'min_length' => 'Le pseudo est trop court. 2 caractères minimum.',
-                                'max_length' => 'Le pseudo est trop long. 15 caractères maximum.',)
-                            ),
-                    new sfValidatorRegex(array('pattern' => '/^[a-zA-Z0-9-]+$/'))
-                    ),
-                array(),
-                array(
-                  'required' => "Le pseudo est requis",
-                  'invalid' => "Le pseudo ne peux pas contenir de caractère spéciaux.")
-                ));
-      $this->setValidator('password', new sfValidatorString(
-              array('required' => true, 'min_length' => 4, 'max_length' => 255),
-              array(
-                'min_length' => "Le mot de passe est trop court, 4 caractères minimum.",
-                'max_length' => "Le mot de passe est trop long, 255 caractères maximum.",
-                'required' => "Le mot de passe est requis",
-                'invalid' => "Le mot de passe doit contenir en entre 4 et 255 caractères")
-              ));
-      $this->setValidator('password_again', new sfValidatorString(
-                                              array('required' => true, 'min_length' => 4, 'max_length' => 20),
-                                              array(
-                                                'min_length' => "Le mot de passe est trop court, 4 caractères minimum.",
-                                                'max_length' => "Le mot de passe est trop long, 255 caractères maximum.",
-                                                'required' => "La confirmation est obligatoire",
-                                                'invalid' => "Le mot de passe doit contenir en entre 4 et 255 caractères")
-                                              ));
 
+    $this->setValidator('first_name', new sfValidatorString(
+      array(
+        'required' => true,
+        'min_length' => 4,
+        'max_length' => 255
+      )
+    ));
+
+
+    $this->setValidator('last_name', new sfValidatorString(
+      array(
+        'required' => true,
+        'min_length' => 4,
+        'max_length' => 255
+      )
+    ));
+
+
+    $this->setValidator('email_address', new sfValidatorEmail(
+      array(
+        'required' => true
+      ),
+      array(
+        'required' => 'L\'adresse de courriel est requise',
+        'invalid' => 'Ce courriel n\'est pas valide'
+      )
+    ));
+
+
+    $this->setValidator('username', new sfValidatorAnd(array(
+      new sfValidatorString(
+        array(
+          'required' => true,
+          'min_length' => 2,
+          'max_length' => 15
+        ),
+        array(
+          'min_length' => 'Le pseudo est trop court. 2 caractères minimum.',
+          'max_length' => 'Le pseudo est trop long. 15 caractères maximum.',
+        )
+      ),
+      new sfValidatorRegex(
+        array(
+          'pattern' => '/^[a-zA-Z0-9-]+$/')
+        )
+      ),
+      array(),
+      array(
+        'required' => 'Le pseudo est requis',
+        'invalid' => 'Le pseudo ne peux pas contenir de caractere speciaux'
+      )
+    ));
+
+
+    $this->setValidator('password', new sfValidatorString(
+      array(
+        'required' => true,
+        'min_length' => 4,
+        'max_length' => 50
+      ),
+      array(
+        'min_length' => 'Le mot de passe est trop court, 4 caracteres minimum.',
+        'max_length' => 'Le mot de passe est trop long, 50 caracteres maximum.',
+        'required' => 'Le mot de passe est requis',
+        'invalid' => 'Le mot de passe doit contenir en entre 4 et 50 caracteres'
+      )
+    ));
+
+
+    $this->setValidator('password_again', new sfValidatorString(
+      array(
+        'required' => true,
+        'min_length' => 4,
+        'max_length' => 20
+      ),
+      array(
+        'min_length' => 'Le mot de passe est trop court, 4 caracteres minimum.',
+        'max_length' => 'Le mot de passe est trop long, 50 caracteres maximum.',
+        'required' => 'La confirmation est obligatoire',
+        'invalid' => 'Le mot de passe doit contenir en entre 4 et 255 caracteres'
+      )
+    ));
+
+
+    $this->setValidator('captcha', new sfValidatorReCaptcha(
+      array(
+        'private_key' => sfConfig::get('app_recaptcha_private_key')
+      )
+    ));
+
+
+    $this->validatorSchema->setPostValidator(new sfValidatorAnd(
+      array(
+        new sfValidatorDoctrineUnique(
+          array(
+            'model' => 'sfGuardUser',
+            'column' => array(
+              'email_address'
+            )
+          )
+        ),
+        new sfValidatorDoctrineUnique(
+          array(
+            'model' => 'sfGuardUser',
+            'column' => array(
+              'username'
+            )
+          )
+        ),
+        new sfValidatorSchemaCompare('password',  sfValidatorSchemaCompare::EQUAL, 'password_again'),
+      ))
+    );
   }
 }
