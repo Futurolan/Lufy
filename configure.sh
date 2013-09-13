463b2e44832e6c5e581ce4bb49207dafd0b0a28c
Guillaume Marsay <gmarsay@gmail.com>
2013-09-13 23:06:19 +0200

    Corrections

--- configure.sh
+++ configure.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-echo "Mise a jour des permissions..."
-
-#chown -R www-data:www-data .
-#chmod -R 755 .
-
-echo "Création du log"
 mkdir -p log
-#chown -R www-data:www-data log/
 chmod -R 777 log/
-
-
-echo "Création du cache"
 mkdir -p cache
-#chown -R www-data:www-data cache/
 chmod -R 777 cache/
-
-
 mkdir -p web/uploads/
 chmod -R 777 web/uploads/
 
 
-echo "OK"
