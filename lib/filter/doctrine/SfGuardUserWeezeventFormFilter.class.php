ce7c7ee45ce48efa8c13415d2bc8dbc121a30ed5
Guillaume Marsay <gmarsay@gmail.com>
2014-01-05 16:07:14 +0100

    * Suppression des anciennes classes * Ajout des classes SfGuardUserWeezevent

--- lib/filter/doctrine/SfGuardUserWeezeventFormFilter.class.php
+++ lib/filter/doctrine/SfGuardUserWeezeventFormFilter.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * PokerTournament filter form.
+ * SfGuardUserWeezevent filter form.
  *
  * @package    lufy
  * @subpackage filter
  * @author     Guillaume Marsay
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class PokerTournamentFormFilter extends BasePokerTournamentFormFilter
+class SfGuardUserWeezeventFormFilter extends BaseSfGuardUserWeezeventFormFilter
 {
   public function configure()
   {
--- lib/form/doctrine/SfGuardUserWeezeventForm.class.php
+++ lib/form/doctrine/SfGuardUserWeezeventForm.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * PokerTournament form.
+ * SfGuardUserWeezevent form.
  *
  * @package    lufy
  * @subpackage form
  * @author     Guillaume Marsay
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class PokerTournamentForm extends BasePokerTournamentForm
+class SfGuardUserWeezeventForm extends BaseSfGuardUserWeezeventForm
 {
   public function configure()
   {
