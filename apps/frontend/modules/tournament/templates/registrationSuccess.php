d891691e1dcbb77a57d73155b59f42e9c0bb61bc
Bardot <Geek_Zone@hotmail.com>
2014-01-31 10:42:17 +0100

    Frontend: -Mise en place de registrationSuccess.

--- apps/frontend/modules/tournament/templates/registrationSuccess.php
+++ apps/frontend/modules/tournament/templates/registrationSuccess.php
@@ -1,18 +1,64 @@
-<h2><?=__('Inscription au tournoi')?> <?php echo $tournament->getName()?></h2>
+<h2><?= __('Inscription au tournoi') ?> <?php echo $tournament->getName() ?></h2>
 
 <div class="row-fluid">
   <div class="span12">
     <div>
-      <?=__('Avant de confirmer votre inscription au tournoi merci de verifier les informations ci-dessous et vous devez accepter de reglement interieur de la manifestation.')?>
+      <?= __('Avant de confirmer votre inscription au tournoi merci de verifier les informations ci-dessous et vous devez accepter de reglement interieur de la manifestation.') ?>
     </div>
-    <br/>
-    <div>...RECAP TEAM, TOURNOI, MONTANT TOTAL, PROCEDURE D'INSCRIPTION...</div>
-    <br/>
-    <div>...LE REGLEMENT ICI DANS UNE DIV SCROLLABLE...</div>
     <br/>
     <div>
-      <?=link_to(__('J accepte le reglement'), 'tournament/registrationConfirm?slug='.$tournament->getSlug().'&team_slug=', array('class' => 'btn btn-success'))?>
-      <?=link_to(__('Je refuse le reglement'), 'tournament/view?slug='.$tournament->getSlug(), array('class' => 'btn btn-danger'))?>
-    </div>
+    <?php $team = $sf_user->getGuardUser()->TeamPlayer[0]->getTeam() ?>
+    <h3><?php echo $team->getName(); ?></h3>
+    <table class="table">
+      <tr>
+        <td align="center" valign="top" rowspan="5" width="200">
+          <img src="<?php echo $team->getLogourl(); ?>" width="200" />
+        </td>
+        <td><?php echo __('Team') ?></td>
+        <td><?php echo $team->getName() ?></td>
+      </tr>
+      <tr>
+        <td><?php echo __('Tag') ?></td>
+        <td><?php echo $team->getTag() ?></td>
+      </tr>
+      <tr>
+        <td><?php echo __('Cree le') ?></td>
+        <td><?php echo $team->getCreatedAt() ?></td>
+      </tr>
+      <tr>
+        <td><?php echo __('Site web') ?></td>
+        <td><?php echo $team->getWebsite() ?></td>
+      </tr>
+      <tr>
+        <td><?php echo __('Description') ?></td>
+        <td><?php echo $team->getdescription() ?></td>
+      </tr>
+    </table>
+    <h3><?php echo __('Composition'); ?></h3>
+    <table class="table">
+      <?php $nbPlayer = 0 ;foreach ($sf_user->getGuardUser()->TeamPlayer[0]->getTeam()->getTeamPlayer() as $player): ?>
+        <tr>
+          <td><?php echo $player->getSfGuardUser()->getUsername(); ?></td>
+          <td><?php echo $player->getSfGuardUser()->getFirstName(); ?> <?php echo substr($player->getSfGuardUser()->getLastName(), 0, 1); ?>.</td>
+          <td>
+            <span class="label"><?php if ($player->getIsCaptain() == 1) echo __('Manager'); ?></span>
+            <span class="label"><?php if ($player->getIsPlayer() == 1) echo __('Joueur'); ?></span>
+          </td>
+          <td><?php echo $tournament->getCostPerPlayer().__(' Euros.') ?></td>
+        </tr>
+        <?php $nbPlayer += 1; ?>
+      <?php endforeach; ?>
+    </table>
+    <h4><?php echo __('Total :').$nbPlayer*$tournament->getCostPerPlayer().' Euros';?></h4>
+    <br/>
+    PROCEDURE D'INSCRIPTION...
   </div>
+  
+  <div>...LE REGLEMENT ICI DANS UNE DIV SCROLLABLE...</div>
+  <br/>
+  <div>
+    <?= link_to(__('J accepte le reglement'), 'tournament/registrationConfirm?slug=' . $tournament->getSlug() . '&team_slug=', array('class' => 'btn btn-success')) ?>
+    <?= link_to(__('Je refuse le reglement'), 'tournament/view?slug=' . $tournament->getSlug(), array('class' => 'btn btn-danger')) ?>
+  </div>
+</div>
 </div>
