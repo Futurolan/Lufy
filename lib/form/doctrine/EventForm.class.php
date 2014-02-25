eb0a96a0cd056474f1b36d715cc8f785885de6f4
Guillaume Marsay <gmarsay@gmail.com>
2014-02-25 20:39:10 +0100

    Correction du form Event

--- lib/form/doctrine/EventForm.class.php
+++ lib/form/doctrine/EventForm.class.php
@@ -18,7 +18,7 @@ class EventForm extends BaseEventForm
 
     $this->widgetSchema['description'] = new sfWidgetFormTextarea(array(), array('rows' => 5, 'cols' => 80));
 
-    $files = sfFinder::type('file')->maxdepth(0)->name('*.jpg')->name('*.png')->relative()->in(sfConfig::get('sf_upload_dir').'/event/logo');
+    $files = sfFinder::type('file')->maxdepth(0)->name('*.jpg')->name('*.png')->relative()->in(sfConfig::get('sf_upload_dir').'/events/logo');
     $logo[0] = '';
     foreach($files as $file)
       {
